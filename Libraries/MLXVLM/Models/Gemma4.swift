8e3d2fa8e6e384174148ebd66efd3b985f991a29
hirenpatelatl <hirenpatelatl@gmail.com>
2026-07-10 07:32:13 -0400

    Fix Gemma 4 VLM shared-layer loading

--- Libraries/MLXVLM/Models/Gemma4.swift
+++ Libraries/MLXVLM/Models/Gemma4.swift
@@ -1087,8 +1087,9 @@ final class Gemma4TextBackbone: Module {
         Gemma4RMSNormZeroShift?
 
     init(_ config: Gemma4TextConfiguration) {
+        let firstSharedLayerIndex = config.hiddenLayers - config.numKVSharedLayers
         self.config = config
-        self.firstKVSharedLayerIdx = config.hiddenLayers - config.numKVSharedLayers
+        self.firstKVSharedLayerIdx = firstSharedLayerIndex
         self.embedScale = pow(Float(config.hiddenSize), 0.5)
         self.embedTokensPerLayerScale = pow(Float(max(config.hiddenSizePerLayerInput, 1)), 0.5)
         self._perLayerInputScale = rsqrt(MLXArray(2.0))
